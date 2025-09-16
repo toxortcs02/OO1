2bef867407141c8cab3144dcd42c15089e6db4e5
Tomas Sabella <tomassabella@hotmail.com>
2025-09-15 22:54:32 -0300

    innit presupuesto2

---
+++ ejercicio3/src/main/java/ar/edu/unlp/objetos/uno/ejercicio3/Presupuesto.java
@@ -0,0 +1,18 @@
+package ar.edu.unlp.objetos.uno.ejercicio3;
+import java.time.LocalDate;
+public class Presupuesto {
+	private LocalDate fecha;
+	private String cliente;
+	
+	
+	
+	
+	public void agregarItem() {
+		
+	}
+	public double calcularTotal() {
+		
+		double x=0;
+		return x;
+	}
+}
